a127f786e1c60f3d94ef98fb1915cce93702d7f3
Andrew <andrew.tian@wustl.edu>
2025-06-18 15:12:00 +0800

    Finished

---
+++ script.js
@@ -0,0 +1,40 @@
+const images = document.querySelectorAll(".image-container img");
+const overlay = document.querySelector(".overlay");
+let current_index = 0;
+
+// click image
+images.forEach((img, index) => {
+    img.addEventListener("click", () => {
+        console.log("Image clicked:", index);
+        current_index = index;
+        store_large_img(current_index);
+        overlay.style.display = "flex";
+        document.body.style.overflow = "hidden";
+    });
+});
+
+// close button
+document.querySelector("#closeBtn").addEventListener("click", close);
+function close() {
+    overlay.style.display = "none";
+    document.body.style.overflow = "";
+}
+
+// previous button
+document.querySelector("#prevBtn").addEventListener("click", prev);
+function prev() {
+    store_large_img(current_index - 1);
+}
+
+// next button
+document.querySelector("#nextBtn").addEventListener("click", next);
+function next() {
+    store_large_img(current_index + 1);
+}
+
+// show image
+function store_large_img(index) {
+    current_index = (index + images.length) % images.length;
+    document.querySelector(".overlay img").src = images[current_index].src;
+}
+
