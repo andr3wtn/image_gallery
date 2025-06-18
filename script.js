3ac506a5b077c3f12ad58aa63e622f2d37cc18df
Andrew <andrew.tian@wustl.edu>
2025-06-18 16:54:00 +0800

    Limit image navigation to same flower section; Changed custom font

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const images = document.querySelectorAll(".image-container img");
+const rose_images = document.querySelectorAll("#rose img");
+const tulip_images = document.querySelectorAll("#tulip img");
+const daisy_images = document.querySelectorAll("#daisy img");
+
 const overlay = document.querySelector(".overlay");
 let current_index = 0;
+let current_section_images = [];
 
-// click image
-images.forEach((img, index) => {
-    img.addEventListener("click", () => {
-        console.log("Image clicked:", index);
-        current_index = index;
-        store_large_img(current_index);
-        overlay.style.display = "flex";
-        document.body.style.overflow = "hidden";
+// open overlay
+function addListener(images){
+    images.forEach((img, index) => {
+        img.addEventListener("click", () => {
+            current_section_images = images;
+            current_index = index;
+            store_large_img(current_index);
+            overlay.style.display = "flex";
+            document.body.style.overflow = "hidden";
+        });
     });
-});
+}
+
+addListener(rose_images);
+addListener(tulip_images);
+addListener(daisy_images);
+
 
 // close button
 document.querySelector("#closeBtn").addEventListener("click", close);
@@ -34,8 +45,8 @@ function next() {
 
 // show image
 function store_large_img(index) {
-    current_index = (index + images.length) % images.length;
-    document.querySelector(".overlay img").src = images[current_index].src;
+    current_index = (index + current_section_images.length) % current_section_images.length;
+    document.querySelector(".overlay img").src = current_section_images[current_index].src;
 }
 
 
